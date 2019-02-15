7cd841543d2e8a4ccc7a155dd6af6109ff5b8445
daiyongbing <1379358788@qq.com>
2019-02-15 09:46:10 +0800

    新建项目

---
+++ src/main/java/com/bing/schat/Schat.java
@@ -0,0 +1,14 @@
+package com.bing.schat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Schat {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Schat.class, args);
+    }
+
+}
+
